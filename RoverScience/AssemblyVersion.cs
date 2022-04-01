bde4427c0b9318dee734a85c6062f5e9a373a3bb
Jonathan Bayer <linuxgurugamer@gmail.com>
2022-04-01 11:11:10 -0400

    csproj change

--- RoverScience/AssemblyVersion.cs
+++ RoverScience/AssemblyVersion.cs
@@ -5,5 +5,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("2.3.5.3")]
- [assembly: AssemblyFileVersion("2.3.5.3")]
+ [assembly: AssemblyVersion("2.3.5.4")]
+ [assembly: AssemblyFileVersion("2.3.5.4")]
